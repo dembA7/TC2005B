d6c35dfaeb90ab845eba13548982e68651057598
dembo <a01709522@tec.mx>
2023-02-16 09:46:28 -0600

    add file.j

--- Laboratorio 2/file.js
+++ Laboratorio 2/file.js
@@ -5,3 +5,15 @@ var comida = "chilaquiles";
 
 let cena = "tacos";
 
+const precio = 70;
+
+console.log("hola");
+console.info("Valor de la comida: " + comida);
+console.warn("El precio no se puede modificar");
+console.error("Te dije que el precio no se puede modificar");
+
+console.assert(1 === 1);
+console.assert(1 === "1");
+console.assert(1 == "1");
+console.assert(1 == true);
+console.assert(1 === true);
