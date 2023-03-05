04567c5a747236da9011bcfbab79903cb5d58cfe
unknown <a01709522@tec.mx>
2023-03-05 15:27:24 -0600

    update js and html files

--- Laboratorio 6 POE/js.js
+++ Laboratorio 6 POE/js.js
@@ -6,3 +6,25 @@
 //
 // =================================================================
 
+
+function validar() {
+    let contrasena = prompt("Ingrese una contrasena: ");
+    let regexMayuscula = /[A-Z]/;
+    let regexMinuscula = /[a-z]/;
+    let regexDigito = /[0-9]/;
+    let regexEspecial = /[\W_]/;
+  
+    if (
+      regexMayuscula.test(contrasena) &&
+      regexMinuscula.test(contrasena) &&
+      regexDigito.test(contrasena) &&
+      regexEspecial.test(contrasena) &&
+      contrasena.length >= 8
+    ) {
+      alert("Contraseña válida.");
+    } else {
+      alert(
+        "Contraseña no válida, vuelva a intentarlo."
+      );
+    }
+  }
