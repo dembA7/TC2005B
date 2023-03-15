50c3ba6f1c18530c27342b9e98ec7b25c212a14e
unknown <a01709522@tec.mx>
2023-03-15 13:34:06 -0600

    update routes files

--- Laboratorio 11 Express/app.js
+++ Laboratorio 11 Express/app.js
@@ -5,7 +5,6 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.json());
 
-<<<<<<< HEAD
 // Rutas
 const mainRuta = require('./routes/main.routes');
 const azulRuta = require('./routes/blue.routes');
@@ -20,33 +19,6 @@ app.use('/amarillo', amarilloRuta);
 
 // 404
 app.use((request, response) => {
-=======
-const azulRuta = require('./routes/blue.routes');
-app.use(azulRuta);
-
-const verdeRuta = require('./routes/green.routes');
-app.use(verdeRuta);
-
-const amarilloRuta = require('./routes/yellow.routes');
-app.use(amarilloRuta);
-
-
-app.use('/',(request, response, next) => {
-    let html = `
-        <!DOCTYPE html>
-        <html>
-        <head><meta charset="utf-8"></head><body>
-        <title>Laboratorio 11 Express</title>
-        <h2>Colores</h2>
-        <p style="color: blue;">Azul<p>
-        <p style="color: green;">Verde<p>
-        <p style="color: #F4D03F;">Amarillo<p>
-    `;
-    response.send(html);
-});
-
-app.use((request, response, next) => {
->>>>>>> d493a96f540a4d4bed2ac51e9c2a7b14dc51ebeb
     response.statusCode = 404;
     response.send("Lo sentimos, no conocemos ese color.");
 });
