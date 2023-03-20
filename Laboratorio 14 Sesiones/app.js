b85b94f838ec3bbdf19fc918b35d32d85d11d12f
unknown <a01709522@tec.mx>
2023-03-20 03:09:20 -0600

    add session

--- Laboratorio 14 Sesiones/app.js
+++ Laboratorio 14 Sesiones/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
 
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: false}));
@@ -8,6 +9,12 @@ app.use(express.json());
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 // app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'xKrsDBW3!!L5tb5w5E$2gANH5W@0d6fV*MQR@e2l47%ADrB@Vd', 
+    resave: false,
+    saveUninitialized: false
+}));
 
 // Rutas
 const mainRuta = require('./routes/main.routes');
