61826089d318784ace8aca8d695867b2c221fea9
unknown <a01709522@tec.mx>
2023-03-20 02:54:54 -0600

    add cookies

--- Laboratorio 14 Sesiones/controllers/main.controller.js
+++ Laboratorio 14 Sesiones/controllers/main.controller.js
@@ -1,30 +1,60 @@
 const Color = require('../models/colors.model');
 
 exports.getMain = (request, response, next) => {
+    
+    let cookies = request.get('Cookie') || '';
+    let mainConsultas = cookies.split(';')[0].split('=')[1] || 0;
+    mainConsultas++;
+    response.setHeader('Set-Cookie', 'mainConsultas=' + mainConsultas + '; HttpOnly');
+
     response.render('main', {
         titulo: 'Colores',
     });
 };
 
 exports.getRegister = (request, response, next) => {
+
+    let cookies = request.get('Cookie') || '';
+    let registerConsultas = cookies.split(';')[0].split('=')[1] || 0;
+    registerConsultas++;
+    response.setHeader('Set-Cookie', 'registerConsultas=' + registerConsultas + '; HttpOnly');
+
     response.render('register', {
         titulo: 'Registrar',
     });
 };
 
 exports.getBlue = (request, response, next) => {
+
+    let cookies = request.get('Cookie') || '';
+    let blueConsultas = cookies.split(';')[0].split('=')[1] || 0;
+    blueConsultas++;
+    response.setHeader('Set-Cookie', 'blueConsultas=' + blueConsultas + '; HttpOnly');
+
     response.render('blue', {
         titulo: 'Azul',
     });
 };
 
 exports.getYellow = (request, response, next) => {
+
+    let cookies = request.get('Cookie') || '';
+    let yellowConsultas = cookies.split(';')[0].split('=')[1] || 0;
+    yellowConsultas++;
+    response.setHeader('Set-Cookie', 'yellowConsultas=' + yellowConsultas + '; HttpOnly');
+
     response.render('yellow', {
         titulo: 'Amarillo',
     });
 };
 
 exports.getGreen = (request, response, next) => {
+
+    let cookies = request.get('Cookie') || '';
+    let greenConsultas = cookies.split(';')[0].split('=')[1] || 0;
+    greenConsultas++;
+    response.setHeader('Set-Cookie', 'greenConsultas=' + greenConsultas + '; HttpOnly');
+
     response.render('green', {
         titulo: 'Verde',
     });
@@ -44,13 +74,3 @@ exports.postColor = (request, response, next) => {
 exports.listar = (request, response, next) => {
     response.render('list', { colors: Color.fetchAll() });
 };
-
-function contador(req, res) {
-    var contador = parseInt(req.cookies.contador || 0);
-    contador++;
-    res.cookie('contador', contador);
-    res.send('Número de consultas: ' + contador);
-    console.log('Número de consultas: ' + contador);
-}  
-
-exports.contador = contador;
