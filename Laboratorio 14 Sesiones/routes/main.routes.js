ec5d72634ce13d60e1dc9d9f45b5ac93c8944d5b
dembo <a01709522@tec.mx>
2023-03-28 11:18:26 -0600

    fix bug

--- Laboratorio 14 Sesiones/routes/main.routes.js
+++ Laboratorio 14 Sesiones/routes/main.routes.js
@@ -6,5 +6,6 @@ const mainController = require('../controllers/main.controller')
 router.get('/', mainController.getMain);
 router.get('/register', mainController.getRegister);
 router.post('/register', mainController.postColor);
+router.get('/list', mainController.listar)
 
 module.exports = router;
