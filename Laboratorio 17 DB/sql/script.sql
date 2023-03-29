f193316171a52a8ebdeb9cfd77f11e44452aaa31
unknown <a01709522@tec.mx>
2023-03-29 17:37:43 -0600

    add db files

--- Laboratorio 17 DB/sql/script.sql
+++ Laboratorio 17 DB/sql/script.sql
@@ -16,7 +16,8 @@ CREATE TABLE colors (
     created_at timestamp NOT NULL DEFAULT current_timestamp()
 );
 
-INSERT INTO colors(nombre, descripcion) VALUES(("Azul", "El color azul es un color muy versátil y puede tener diferentes simbolismos dependiendo del contexto cultural y personal en el que se encuentre."),
-    ("Amarillo", "El color amarillo es un color muy vibrante y llamativo que se asocia a menudo con la alegría, la energía y la felicidad. Simboliza la luz del sol, la creatividad y la inteligencia."),
-    ("Verde", "El color verde es a menudo asociado con la naturaleza, la frescura y la vida. Simboliza la esperanza, la armonía y el equilibrio, evocando sentimientos de paz y tranquilidad.");
-);
+INSERT INTO colors(nombre, descripcion) VALUES
+    ('Azul', 'El color azul es un color muy versátil y puede tener diferentes simbolismos dependiendo del contexto cultural y personal en el que se encuentre.'),
+    ('Amarillo', 'El color amarillo es un color muy vibrante y llamativo que se asocia a menudo con la alegría, la energía y la felicidad. Simboliza la luz del sol, la creatividad y la inteligencia.'),
+    ('Verde', 'El color verde es a menudo asociado con la naturaleza, la frescura y la vida. Simboliza la esperanza, la armonía y el equilibrio, evocando sentimientos de paz y tranquilidad.');
+
