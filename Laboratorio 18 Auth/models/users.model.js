d0a44726eb6d3a614edf92474c4ae861328c0c2b
unknown <a01709522@tec.mx>
2023-03-29 23:29:21 -0600

    add password encryption

--- Laboratorio 18 Auth/models/users.model.js
+++ Laboratorio 18 Auth/models/users.model.js
@@ -1,4 +1,5 @@
 const db = require('../util/database');
+const bcrypt = require('bcryptjs');
 
 module.exports = class User {
 
@@ -9,9 +10,13 @@ module.exports = class User {
     }
 
     save() {
-        return db.execute(`
-            INSERT INTO usuarios (nombre, username, password)
+        return bcrypt.hash(this.password, 12)
+        .then((encryptedPassword) => {
+            return db.execute(`
+                INSERT INTO usuarios (nombre, username, password)
             values (?, ?, ?)
-        `, [this.nombre, this.username, this.password]);
+            `, [this.nombre, this.username, encryptedPassword]);
+        })
+        .catch((error) => {console.log(error)});
     }
 }
