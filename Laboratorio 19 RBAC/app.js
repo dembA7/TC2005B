1fc57f6dba801d6d72978b2aeb1ef3c95c44dbe6
dembo <a01709522@tec.mx>
2023-03-30 08:09:50 -0600

    initialize project

---
+++ Laboratorio 19 RBAC/app.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const session = require('express-session');
+const isAuth = require('./util/is-auth');
+const csrf = require('csurf');
+
+const bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(express.json());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+// app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+    secret: 'xKrsDBW3!!L5tb5w5E$2gANH5W@0d6fV*MQR@e2l47%ADrB@Vd', 
+    resave: false,
+    saveUninitialized: false
+}));
+
+//CSRF Protection
+const csrfProtection = csrf();
+app.use(csrfProtection); 
+app.use((request, response, next) => {
+    response.locals.csrfToken = request.csrfToken();
+    next();
+});
+
+// Rutas
+const mainRuta = require('./routes/main.routes');
+const azulRuta = require('./routes/blue.routes');
+const verdeRuta = require('./routes/green.routes');
+const amarilloRuta = require('./routes/yellow.routes');
+const registrarRuta = require('./routes/main.routes');
+const usersRuta = require('./routes/users.routes')
+
+// Middlewares
+app.use('/', mainRuta);
+app.use('list', isAuth, mainRuta)
+app.use(azulRuta);
+app.use(verdeRuta);
+app.use(amarilloRuta);
+app.use(registrarRuta);
+app.use(usersRuta);
+
+// 404
+app.use((request, response) => {
+    response.statusCode = 404;
+    response.send("Lo sentimos, no conocemos ese color.");
+});
+
+app.listen(3000);
---
+++ Laboratorio 19 RBAC/models/users.model.js
@@ -0,0 +1,30 @@
+const db = require('../util/database');
+const bcrypt = require('bcryptjs');
+
+module.exports = class User {
+
+    constructor(newUser) {
+        this.nombre = newUser.nombre || 'John Doe';
+        this.username = newUser.username || 'johndoe';
+        this.password = newUser.password || 'johndoejohndoe';
+    }
+
+    save() {
+        return bcrypt.hash(this.password, 12)
+        .then((encryptedPassword) => {
+            return db.execute(`
+                INSERT INTO users (nombre, username, password)
+                values (?, ?, ?)
+            `, [this.nombre, this.username, encryptedPassword]);
+        })
+        .catch((error) => {console.log(error)});
+    }
+
+    static fetchOne(username){
+        return db.execute(`
+            SELECT * 
+            FROM users
+            WHERE username = ?
+        `, [username]);
+    }
+}
