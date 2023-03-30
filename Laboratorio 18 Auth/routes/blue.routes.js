c9f06442258c163e0fa896824528e1db32b97ce5
unknown <a01709522@tec.mx>
2023-03-29 21:02:45 -0600

    remove project

--- Laboratorio 18 Auth/routes/blue.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const mainController = require('../controllers/main.controller')
-
-router.get('/azul', mainController.getBlue);
-
-module.exports = router;
--- Laboratorio 18 Auth/routes/green.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const mainController = require('../controllers/main.controller')
-
-router.get('/verde', mainController.getGreen);
-
-module.exports = router;
